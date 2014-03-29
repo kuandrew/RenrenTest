2770ffc222387c5d0d88fff38564cea865abd70d
Ku_Andrew <kungg@kungg.com>
2014-03-30 01:03:50 +0800

    1.0
    1.0

---
+++ post.php
@@ -0,0 +1,45 @@
+<?php
+$post = $_POST;
+$timestamp = time();
+$nonce = rand(0,912312);
+setcookie('url',$post['url']);
+setcookie('token',$post['token']);
+setcookie('fromUser',$post['fromUser']);
+setcookie('toUser',$post['toUser']);
+$data[0] = $post['token'];
+$data[1] = $timestamp;
+$data[2] = $nonce;
+sort($data,SORT_LOCALE_STRING);
+$result = $data[1].$data[0].$data[2];
+$signature = strtoupper(sha1($result));
+$message = "<xml>
+				    <ToUser><![CDATA[%s]]></ToUser>
+				    <FromUser><![CDATA[%s]]></FromUser>
+                    <CreateTime>%s</CreateTime>
+                    <MsgType><![CDATA[%s]]></MsgType>
+                    <SmallUrl><![CDATA[%s]]></SmallUrl>
+                    <LargeUrl><![CDATA[%s]]></LargeUrl>
+                    <Content><![CDATA[%s]]></Content>
+				    </xml>";
+if($post['largeurl'] || $post['smallurl']) $msgType = 'image';
+$message = sprintf($message, $post['toUser'], $post['fromUser'], time(), $msgType,$post['smallurl'],$post['largeurl'],$post['content']);
+$data = array(
+	'timestamp' => $timestamp,
+	'nonce' => $nonce,
+	'signature' => $signature,
+	'message' => $message
+	);
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $post['url']);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+// 我们在POST数据哦！
+curl_setopt($ch, CURLOPT_POST, 1);
+// 把post的变量加上
+curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+$output = curl_exec($ch);
+curl_close($ch);
+@$result  = simplexml_load_string($output, 'SimpleXMLElement', LIBXML_NOCDATA);
+echo '<p>'.$result->Content.'</p>';
+echo '<p>'.$output.'</p>';
+
+?>
---
+++ test.php
@@ -0,0 +1,40 @@
+<html>
+<head>
+<meta charset="utf8"></meta>
+<title>test</title>
+<script src="http://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.7.2.min.js"></script>
+</head>
+<body>
+	
+		rest地址<input id="url" name="url" value="<?php echo $_COOKIE['url'] ?>"/></br>
+		TOKEN<input id="token" name="token" value="<?php echo $_COOKIE['token'] ?>" /></br>
+		fromUser<input id="fromUser" name="fromUser" value="<?php echo $_COOKIE['fromUser'] ?>" /></br>
+		toUser<input id="toUser" name="toUser" value="<?php echo $_COOKIE['toUser'] ?>" /></br>
+		smallurl<input id="smallurl" name="smallurl" value="<?php echo $_COOKIE['smallurl'] ?>" /></br>
+		largeurl<input id="largeurl" name="largeurl" value="<?php echo $_COOKIE['largeurl'] ?>" /></br>
+		内容<input id="content" name="content" /></br>
+		你说：<p id="say"></p>
+		回复：<div id="response"></div>
+		<input type="submit" onclick="post()" value="提交" />
+		
+<script type="text/javascript">
+function post(){
+	$('#response').empty();
+	$('#say').text($('#content').val());
+	$.post('post.php',
+  {
+  	url:$('#url').val(),
+  	token:$('#token').val(),
+  	fromUser:$('#fromUser').val(),
+  	toUser:$('#toUser').val(),
+  	smallurl:$('#smallurl').val(),
+  	largeurl:$('#largeurl').val(),
+  	content:$('#content').val(),
+  },
+  function(data,status){
+  	$('#response').append(data);
+  });
+}
+</script>	
+</body>
+</html>
